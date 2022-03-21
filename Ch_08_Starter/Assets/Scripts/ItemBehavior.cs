84bef1e68ef45913674ffe3823ef9418ed4fd237
adsyde <tagir85@gmail.com>
2022-03-21 19:07:07 +0300

    Learned 9 chapter

--- Ch_08_Starter/Assets/Scripts/ItemBehavior.cs
+++ Ch_08_Starter/Assets/Scripts/ItemBehavior.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
- public class ItemBehavior : MonoBehaviour 
- {
-       // 1
-     void OnCollisionEnter(Collision collision)
-     {
-         // 2
-         if(collision.gameObject.name == "Player")
-         {
-             // 3
-             Destroy(this.transform.parent.gameObject);
+public class ItemBehavior : MonoBehaviour
+{
+    public GameBehavior gameManager;
 
-             // 4
-             Debug.Log("Item collected!");
-         }
-     }
- }
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // 2
+        if (collision.gameObject.name == "Player")
+        {
+            // 3
+            Destroy(this.transform.parent.gameObject);
+
+            // 4
+            gameManager.Items += 1;
+        }
+    }
+}
--- Ch_08_Starter/Assets/Scripts/PlayerBehavior.cs
+++ Ch_08_Starter/Assets/Scripts/PlayerBehavior.cs
@@ -2,50 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBehavior : MonoBehaviour 
- {
-   public float moveSpeed = 10f;
-   public float rotateSpeed = 75f;
- 
-   private float vInput;
-   private float hInput;
- 
-    // 1
-         private Rigidbody _rb;
- 
-    // 2
-         void Start()
-         {
-             // 3
-             _rb = GetComponent<Rigidbody>();
-         }
- 
-     void Update()
-     {
-       vInput = Input.GetAxis("Vertical") * moveSpeed;
-       hInput = Input.GetAxis("Horizontal") * rotateSpeed;
-    
-
-       /* 4
-       this.transform.Translate(Vector3.forward * vInput * 
-       Time.deltaTime);
-       this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
-       */
-     }
-
-     // 1
-void FixedUpdate()
+public class PlayerBehavior : MonoBehaviour
 {
-        // 2
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;    
+    public LayerMask groundLayer;
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
+    private float vInput;
+    private float hInput;
+
+    private Rigidbody _rb;
+    private CapsuleCollider _col;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+
+        /* 4
+        this.transform.Translate(Vector3.forward * vInput * 
+        Time.deltaTime);
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */
+    }
+
+    // 1
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+        }
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+
         Vector3 rotation = Vector3.up * hInput;
 
-        // 3
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-        // 4
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
 
-        // 5
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
+    }
 }
- } 
