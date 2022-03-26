6c45c332c876500dd500cf1f4fe216206fe18bcd
adsyde <tagir85@gmail.com>
2022-03-26 17:48:30 +0300

    Learned ch.13

--- Ch_12_Starter/Assets/Scripts/GameBehavior.cs
+++ Ch_12_Starter/Assets/Scripts/GameBehavior.cs
@@ -7,49 +7,51 @@ using CustomExtensions;
 public class GameBehavior : MonoBehaviour, IManager
 {
     private string _state;
- 
-    public string State 
+
+    public string State
     {
-    get { return _state; }
-    set { _state = value; }
+        get { return _state; }
+        set { _state = value; }
     }
 
     public string labelText = "Collect all 4 items and win your freedom!";
-     
-    public int maxItems = 4;
+
+    public int maxItems = 1;
     public bool showWinScreen = false;
     public bool showLossScreen = false;
 
     public Stack<string> lootStack = new Stack<string>();
 
     private int _itemsCollected = 0;
-     public int Items
-     {
+    public int Items
+    {
         get { return _itemsCollected; }
-        set { 
-            _itemsCollected = value; 
+        set
+        {
+            _itemsCollected = value;
             //Debug.LogFormat("Items: {0}", _itemsCollected);
 
-             if(_itemsCollected >= maxItems)
-             {
+            if (_itemsCollected >= maxItems)
+            {
                 labelText = "You've found all the items!";
                 showWinScreen = true;
                 Time.timeScale = 0f;
-             }                               
-             else
-             {
+            }
+            else
+            {
                 labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
-             }
+            }
         }
     }
- 
-     private int _playerHP = 10;
-    public int HP 
+
+    private int _playerHP = 1;
+    public int HP
     {
-    get { return _playerHP; }
-    set { 
-        _playerHP = value; 
-        if(_playerHP <= 0)
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            if (_playerHP <= 0)
             {
                 labelText = "You want another life with that?";
                 showLossScreen = true;
@@ -62,16 +64,28 @@ public class GameBehavior : MonoBehaviour, IManager
         }
     }
 
+    public delegate void DebugDelegate(string newText);
+    public DebugDelegate debug = Print;
+
     void Start()
     {
         Initialize();
+        InventoryList<string> inventoryList = new InventoryList<string>();
+        inventoryList.SetItem("Potion");
+        Debug.Log(inventoryList.item);
     }
- 
-     public void Initialize() 
+
+    public void Initialize()
     {
         _state = "Manager initialized..";
         _state.FancyDebug();
-        Debug.Log(_state);
+        debug(_state);
+
+        LogWithDelegate(debug);
+
+        GameObject player = GameObject.Find("Player");
+        PlayerBehavior playerBegavior = player.GetComponent<PlayerBehavior>();
+        playerBegavior.playerJump += HandlePlayerJump;
 
         lootStack.Push("Sword of Doom");
         lootStack.Push("HP+");
@@ -80,29 +94,17 @@ public class GameBehavior : MonoBehaviour, IManager
         lootStack.Push("Mythril Bracers");
     }
 
-    void OnGUI()
+    public void HandlePlayerJump()
     {
-        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
-        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
-        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);    
-
-        if (showWinScreen)
-        {
-            if (GUI.Button(new Rect(Screen.width/2 - 100,    
-            Screen.height/2 - 50, 200, 100), "YOU WON!"))
-            {
-                Utilities.RestartLevel(0);
-            }
-        }
-
-        if(showLossScreen)
-        {
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, 
-            Screen.height / 2 - 50, 200, 100), "You lose..."))
-            {
-                Utilities.RestartLevel();
-            }
-        }
+        debug("Player has jumped...");
+    }
+    public static void Print(string newText)
+    {
+        Debug.Log(newText);
+    }
+    public void LogWithDelegate(DebugDelegate del)
+    {
+        del("Delegating the debug task...");
     }
 
     public void PrintLootReport()
@@ -111,5 +113,43 @@ public class GameBehavior : MonoBehaviour, IManager
         var nextItem = lootStack.Peek();
         Debug.LogFormat("You got a {0}! Keep going, you've got a good chance of finding a {1} next.", currentItem, nextItem);
         Debug.LogFormat("There are {0} random loot items waiting for you!", lootStack.Count);
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100,
+            Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+            {
+                Utilities.RestartLevel(0);
+            }
+        }
+
+        if (showLossScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100,
+            Screen.height / 2 - 50, 200, 100), "You lose..."))
+            {
+                try
+                {
+                    Utilities.RestartLevel(-1);
+                    debug("Level restarted successfully...");
+                }
+                catch (System.ArgumentException e)
+                {
+                    Utilities.RestartLevel(0);
+                    debug("Reverting to scene 0:" + e.ToString());
+                }
+                finally
+                {
+                    debug("Restart handled...");
+                }
+            }
+        }
     }
 }
--- Ch_12_Starter/Assets/Scripts/PlayerBehavior.cs
+++ Ch_12_Starter/Assets/Scripts/PlayerBehavior.cs
@@ -3,81 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
- {
-     public float moveSpeed = 10f;
-     public float rotateSpeed = 75f;
-     public float jumpVelocity = 5f;
-     public float distanceToGround = 0.1f;
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
 
-     public LayerMask groundLayer;
+    public LayerMask groundLayer;
 
-     public GameObject bullet;
-     public float bulletSpeed = 100f;
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
 
-     private CapsuleCollider _col;
+    private CapsuleCollider _col;
 
-     private float vInput;
-     private float hInput;
+    private float vInput;
+    private float hInput;
 
-     private Rigidbody _rb;
+    private Rigidbody _rb;
 
-     private GameBehavior _gameManager;
+    private GameBehavior _gameManager;
 
-     void Start()
-     {
-          _rb = GetComponent<Rigidbody>();
-          _col = GetComponent<CapsuleCollider>();
+    public delegate void JumpingEvent();
+    public event JumpingEvent playerJump;
 
-          _gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
-     }
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
 
-     void Update()
-     {
-          vInput = Input.GetAxis("Vertical") * moveSpeed;
-          hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+    }
 
-          /* 4
-          this.transform.Translate(Vector3.forward * vInput *
-          Time.deltaTime);
-          this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
-          */
-     }
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-     void FixedUpdate()
-     {
-          Vector3 rotation = Vector3.up * hInput;
-          Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        /* 4
+        this.transform.Translate(Vector3.forward * vInput *
+        Time.deltaTime);
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */
+    }
 
-          _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
-          _rb.MoveRotation(_rb.rotation * angleRot);
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-          if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-          {
-               _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
-          }
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
 
-          if (Input.GetMouseButtonDown(0))
-         {
-             GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
-             Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            playerJump();
+        }
 
-             bulletRB.velocity = this.transform.forward * bulletSpeed;
-         }
-     }
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
 
-     private bool IsGrounded()
-     {
-         Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
-         bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+        }
+    }
 
-         return grounded;
-     }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
 
-     void OnCollisionEnter(Collision collision)
-     {
-         if(collision.gameObject.name == "Enemy")
-         {
-             _gameManager.HP -= 1;
-         }
-     }
+        return grounded;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Enemy")
+        {
+            _gameManager.HP -= 1;
+        }
+    }
 }
